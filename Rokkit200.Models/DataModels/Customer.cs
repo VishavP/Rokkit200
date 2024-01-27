31add1b38669005b80dbf823658636f84b3e1d56
Vishav Premlall <vishav.premlall@firsttech.digital>
2024-01-27 17:16:28 +0200

    fixed as per requirements

--- Rokkit200.Models/DataModels/Customer.cs
+++ Rokkit200.Models/DataModels/Customer.cs
@@ -3,37 +3,20 @@
     public class Customer
     {
         public string customerNum { get; set; }
-        public ulong balance { get; set; }
-        public ulong overdraft { get; set; }    
+        public long balance { get; set; }
+        public long overdraft { get; set; }    
     }
 
     public class SystemDb
     {
-
         public List<Customer> customers { get; set; }
         private SystemDb()
         {
             customers = new List<Customer>();
-        }
-
-        public void InitCustomers()
-        {
-            var rng = new Random();
-            ulong randomNumber = NextULong(rng,1000000000000000,9999999999999999999);
-            customers.Add(new Customer(){
-                customerNum = Guid.NewGuid().ToString(), 
-                balance= randomNumber, 
-                overdraft=randomNumber});
-        }
-
-        public ulong NextULong(Random self, ulong min, ulong max)
-        {
-            var buf = new byte[sizeof(ulong)];
-            self.NextBytes(buf);
-            ulong n = BitConverter.ToUInt64(buf, 0);
-            double normalised = n / (ulong.MaxValue + 1.0);
-            double range = (double)max - min;
-            return (ulong)(normalised * range) + min;
+            customers.Add(new Customer { customerNum="1", balance=2000 });
+            customers.Add(new Customer { customerNum = "2", balance = 5000 });
+            customers.Add(new Customer { customerNum = "3", balance = 1000, overdraft = 1000 });
+            customers.Add(new Customer { customerNum = "4", balance = -5000, overdraft = 20000 });
         }
     }
 }
