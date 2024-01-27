fe7828fe3c0db9ca19b26511e49aaf1c94dc6d70
Vishav Premlall <vishav.premlall@firsttech.digital>
2024-01-27 16:51:05 +0200

    plumbing - adding projects

---
+++ Rokkit200.Db/customer.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[customer]
+(
+	[customerNum] BIGINT NOT NULL PRIMARY KEY, 
+    [balance] NVARCHAR(50) NOT NULL
+)
---
+++ Rokkit200.Models/DataModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace Rokkit200.Models.DataModels
+{
+    public class Customer
+    {
+        public string customerNumber { get; set; }
+        public Int64 balance { get; set; }
+    }
+}
